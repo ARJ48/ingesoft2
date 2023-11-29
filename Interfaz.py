1eb0bc0c2dffbd2f24f8a0853c2bad401d3e2fce
ARJ48 <alejandro.ramirez3@utp.edu.co>
2023-11-29 01:21:28 -0500

    proyecto acabado(??

--- Interfaz.py
+++ Interfaz.py
@@ -4,8 +4,31 @@ import tkinter.ttk as tk
 from tkinter import messagebox
 from ttkthemes import ThemedStyle
 import pandas as pd
+import numpy as np
+import re
+# Importamos la función para limpiar datos
+from script_limpieza import limpiar_datos
 
-import csv
+
+def descargar_csv(df):
+    df.to_csv('db_organizada.csv', index=False)
+
+
+def ventana_datos(df):
+    ventana = Toplevel(Miventana)
+    ventana.title("Limpieza datos")
+    ventana.geometry("500x500")
+    title = tk.Label(ventana, text="LIMPIANDO DATOS, ESPERE...", font=("Tahoma", 15, "bold"))
+    title.pack(ipady=25)
+
+    new_df = limpiar_datos(df)
+
+    boton_descargar = Button(ventana, text="Depurar datos", command=lambda: descargar_csv(new_df))
+    boton_descargar.pack()
+
+
+
+
 
 def buscarArchivo():
     archivo_abierto = filedialog.askopenfilename(
@@ -33,17 +56,18 @@ def mostrar_columnas(archivo_csv):
         # Crear una nueva ventana para mostrar etiquetas y selectores
         ventana_selectores = Toplevel(Miventana)
         ventana_selectores.title("Selectores de Columnas")
+        ventana_selectores.geometry("500x500")
 
         # Crear etiquetas y selectores
         textos = [
-            "Cedula",
-            "Primer_nombre",
-            "Segundo_nombre",
-            "Primer_apellido",
-            "Segundo_apellido",
-            "Direccion",
-            "Telefono",
-            "Correo"
+            "Cedula:",
+            "Primer nombre:",
+            "Segundo nombre:",
+            "Primer apellido:",
+            "Segundo apellido:",
+            "Direccion:",
+            "Telefono:",
+            "Correo:"
         ]
 
         opciones = {}
@@ -60,34 +84,39 @@ def mostrar_columnas(archivo_csv):
             opciones[texto].pack(side=LEFT)
 
         # Función para obtener las opciones seleccionadas
-        def obtener_opciones():
+        def obtener_opciones(df):
             # Usar la palabra clave global para acceder a variables globales
             global selected_cedula, selected_primer_nombre, selected_segundo_nombre
             global selected_primer_apellido, selected_segundo_apellido, selected_direccion
             global selected_telefono, selected_correo
 
             # Asignar los valores seleccionados a las variables correspondientes
-            selected_cedula = opciones["Cedula"].get()
-            selected_primer_nombre = opciones["Primer_nombre"].get()
-            selected_segundo_nombre = opciones["Segundo_nombre"].get()
-            selected_primer_apellido = opciones["Primer_apellido"].get()
-            selected_segundo_apellido = opciones["Segundo_apellido"].get()
-            selected_direccion = opciones["Direccion"].get()
-            selected_telefono = opciones["Telefono"].get()
-            selected_correo = opciones["Correo"].get()
+            selected_cedula = opciones["Cedula:"].get()
+            selected_primer_nombre = opciones["Primer nombre:"].get()
+            selected_segundo_nombre = opciones["Segundo nombre:"].get()
+            selected_primer_apellido = opciones["Primer apellido:"].get()
+            selected_segundo_apellido = opciones["Segundo apellido:"].get()
+            selected_direccion = opciones["Direccion:"].get()
+            selected_telefono = opciones["Telefono:"].get()
+            selected_correo = opciones["Correo:"].get()
 
-            print("Opciones obtenidas:")
-            print(f"Cedula: {selected_cedula}")
-            print(f"Primer_nombre: {selected_primer_nombre}")
-            print(f"Segundo_nombre: {selected_segundo_nombre}")
-            print(f"Primer_apellido: {selected_primer_apellido}")
-            print(f"Segundo_apellido: {selected_segundo_apellido}")
-            print(f"Direccion: {selected_direccion}")
-            print(f"Telefono: {selected_telefono}")
-            print(f"Correo: {selected_correo}")
+            # print("Opciones obtenidas:")
+            # print(f"Cedula: {selected_cedula}")
+            # print(f"Primer nombre: {selected_primer_nombre}")
+            # print(f"Segundo nombre: {selected_segundo_nombre}")
+            # print(f"Primer apellido: {selected_primer_apellido}")
+            # print(f"Segundo apellido: {selected_segundo_apellido}")
+            # print(f"Direccion: {selected_direccion}")
+            # print(f"Telefono: {selected_telefono}")
+            # print(f"Correo: {selected_correo}")
+
+            ## Coso para limpiar datos RE IMPROVISADO
+            ventana_selectores.destroy()
+            ventana_datos(df)
+
 
         # Botón para obtener las opciones seleccionadas
-        boton_obtener = Button(ventana_selectores, text="Obtener Opciones", command=obtener_opciones)
+        boton_obtener = Button(ventana_selectores, text="Depurar datos", command=lambda: obtener_opciones(df))
         boton_obtener.pack()
 
 
